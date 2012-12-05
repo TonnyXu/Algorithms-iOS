73e0d17dd74d7442212e44710c7ac17237deec3f
Yi Gu <ghawk.gu@gmail.com>
2012-12-05 09:37:49 +0900

    Add a simple implementation of heap.

---
+++ AlgorithmObjC/AlgorithmObjC/CLRSHeap.h
@@ -0,0 +1,25 @@
+//
+//  CLRSHeap.h
+//  AlgorithmObjC
+//
+//  Created by Yi Gu on 12/4/12.
+//  Copyright (c) 2012 Tonny Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum:NSUInteger {
+    MAX_HEAP,
+    MIN_HEAP,
+}HeapType;
+
+@interface CLRSHeap : NSObject
+@property (readonly) NSUInteger size;
+
+- (id)initWithType:(HeapType)type andCapacity:(NSUInteger)capacity andComparator:(NSComparator)comparator;
+
+- (BOOL)insert:(id)object;
+- (id)head;
+- (id)extract;
++ (void)sort:(NSMutableArray *)array withComparator:(NSComparator)comparator;
+@end
---
+++ AlgorithmObjC/AlgorithmObjC/CLRSHeap.m
@@ -0,0 +1,149 @@
+//
+//  CLRSHeap.m
+//  AlgorithmObjC
+//
+//  Created by Yi Gu on 12/4/12.
+//  Copyright (c) 2012 Tonny Xu. All rights reserved.
+//
+
+#import "CLRSHeap.h"
+
+@interface CLRSHeap ()
+@property (assign) HeapType type;
+@property (assign) NSUInteger capacity;
+@property (assign) NSUInteger size;
+@property (strong) NSMutableArray *storage;
+@property (copy) NSComparator comparator;
+@property (assign) NSComparisonResult comparisonResult;
+
+- (void)heapify:(NSUInteger)index;
+- (void)buildHeap;
+- (NSUInteger)parent:(NSUInteger)childIndex;
+- (NSUInteger)left:(NSUInteger)parentIndex;
+- (NSUInteger)right:(NSUInteger)parentIndex;
+- (void)swapObjectAtIndex:(NSUInteger)i withObjectAtIndex:(NSUInteger)j;
+@end
+
+@implementation CLRSHeap
+- (id)initWithType:(HeapType)type andCapacity:(NSUInteger)capacity andComparator:(NSComparator)comparator {
+    self = [super init];
+    if (self) {
+        self.size = 0;
+        self.type = type;
+        self.comparisonResult = type == MAX_HEAP ? NSOrderedDescending : NSOrderedAscending;
+        self.capacity = capacity;
+        self.storage = [NSMutableArray arrayWithCapacity:self.capacity];
+        self.comparator = comparator;
+    }
+
+    return self;
+}
+
+- (id)initWithType:(HeapType)type andArray:(NSMutableArray *)array andComparator:(NSComparator)comparator {
+    self = [super init];
+    if (self) {
+        self.size = [array count];
+        self.type = type;
+        self.comparisonResult = type == MAX_HEAP ? NSOrderedDescending : NSOrderedAscending;
+        self.capacity = [array count];
+        self.storage = array;
+        self.comparator = comparator;
+
+        [self buildHeap];
+    }
+
+    return self;
+}
+
+#pragma mark - internal methods
+- (NSUInteger)parent:(NSUInteger)childIndex {
+    return childIndex == 0 ? 0 : (childIndex - 1) / 2;
+}
+
+- (NSUInteger)left:(NSUInteger)parentIndex {
+    return parentIndex * 2 + 1;
+}
+
+- (NSUInteger)right:(NSUInteger)parentIndex {
+    return [self left:parentIndex] + 1;
+}
+
+- (void)swapObjectAtIndex:(NSUInteger)i withObjectAtIndex:(NSUInteger)j {
+    id temp = self.storage[i];
+    self.storage[i] = self.storage[j];
+    self.storage[j] = temp;
+}
+
+- (void)heapify:(NSUInteger)index {
+    NSUInteger i = index;
+    NSUInteger left = [self left:i];
+    NSUInteger right = left + 1;
+
+    if (left < self.size && self.comparator(self.storage[left], self.storage[i]) == self.comparisonResult ) {
+        i = left;
+    }
+
+    if (right < self.size && self.comparator(self.storage[right], self.storage[i]) == self.comparisonResult ) {
+        i = right;
+    }
+
+    if (i != index) {
+        [self swapObjectAtIndex:index withObjectAtIndex:i];
+        [self heapify:i];
+    }
+}
+
+- (void)buildHeap {
+    NSUInteger last = self.size - 1;
+    for (NSUInteger i = [self parent:last]; i > 0 ; i--) {
+        [self heapify:i];
+    }
+    [self heapify:0];
+}
+
+#pragma mark - public methods
+- (BOOL)insert:(id)object {
+    if (self.size == self.capacity) return NO;
+
+    NSUInteger index = self.size;
+    NSUInteger parentIndex = [self parent:index];
+    self.storage[index] = object;
+    self.size++;
+
+
+    while (self.comparator(self.storage[index], self.storage[parentIndex]) == self.comparisonResult ) {
+        [self swapObjectAtIndex:index withObjectAtIndex:parentIndex];
+        index = parentIndex;
+        parentIndex = [self parent:index];
+    }
+
+    return YES;
+}
+
+- (id)head {
+    return self.size > 0 ? self.storage[0] : nil;
+}
+
+- (id)extract {
+    if (self.size == 0) return nil;
+
+    id object = self.storage[0];
+
+    self.storage[0] = self.storage[self.size - 1];
+    self.size--;
+
+    [self heapify:0];
+
+    return object;
+}
+
++ (void)sort:(NSMutableArray *)array withComparator:(NSComparator)comparator {
+    CLRSHeap *heap = [[CLRSHeap alloc] initWithType:MIN_HEAP andArray:array andComparator:comparator];
+
+    while (heap.size > 0) {
+        [heap swapObjectAtIndex:0 withObjectAtIndex:heap.size - 1];
+        heap.size--;
+        [heap heapify:0];
+    }
+}
+@end
