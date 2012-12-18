b4046a7164cfd34108ed917d97f64aa78f053568
Tonny Xu <tonny.xu@gmail.com>
2012-12-18 18:32:52 +0900

    Added test project. removed old workspace settings.

---
+++ AlgorithmsObjCTest/AlgorithmsObjCTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AlgorithmsObjCTest
+//
+//  Created by 徐 廷 on 2012/12/18.
+//  Copyright (c) 2012年 Tonny Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
